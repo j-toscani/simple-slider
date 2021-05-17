519771718e1ab6bd2453a8f70e130a4df03bf8db
Julian Toscani <julian_toscani@gmx.de>
2021-05-18 00:55:09 +0200

    format files

--- component/sliderStructure.js
+++ component/sliderStructure.js
@@ -1,59 +1,46 @@
-
-
 const leftButton = {
-    elementName: 'button',
-    attrs: {
-        class: "slider-button left",
-        'data-slider' : 'button',
-        text: 'left'
-    },
-    children: [
-        'left'
-    ]
-}
+  elementName: "button",
+  attrs: {
+    class: "slider-button left",
+    "data-slider": "button",
+    text: "left",
+  },
+  children: ["left"],
+};
 
 const rightButton = {
-    elementName: 'button',
-    attrs: {
-        class: "slider-button right",
-        'data-slider' : 'button',
-        innerHTML: 'right'
-    },
-    children: [
-        'right'
-    ]
-}
+  elementName: "button",
+  attrs: {
+    class: "slider-button right",
+    "data-slider": "button",
+    innerHTML: "right",
+  },
+  children: ["right"],
+};
 
 const track = {
-    elementName: 'ul',
-    attrs: {
-        class: 'track',
-        'data-slider': "track",
-        style: "--position: 0"
-    },
-
-}
+  elementName: "ul",
+  attrs: {
+    class: "track",
+    "data-slider": "track",
+    style: "--position: 0",
+  },
+};
 
 const slider = {
-    elementName: 'div',
-    attrs: {
-        class: "slider",
-    },
-    children: [
-        track
-    ]
-}
+  elementName: "div",
+  attrs: {
+    class: "slider",
+  },
+  children: [track],
+};
 
 const SLIDER_STRUCTURE = {
-    elementName: 'div',
-    attrs: {
-        class: 'slider-wrapper'
-    },
-    children: [
-        leftButton,
-        rightButton,
-        slider
-    ]
-}
+  elementName: "div",
+  attrs: {
+    class: "slider-wrapper",
+  },
+  children: [leftButton, rightButton, slider],
+};
 
-export default SLIDER_STRUCTURE
+export default SLIDER_STRUCTURE;
