c2ce8fb691b87b50851d51867676c5ccdabfbb91
Julian Toscani <julian_toscani@gmx.de>
2021-05-18 00:11:28 +0200

    register component

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import setUpFiniteSlider from "./lib/setUpFiniteSlider.js";
+import Slider from './component/Slider.js';
+import setUpFiniteSlider from './lib/setUpFiniteSlider.js';
 
+customElements.define('custom-slider', Slider);
 setUpFiniteSlider();
